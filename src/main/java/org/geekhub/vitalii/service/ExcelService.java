2c0cdac28205052661422120259521206df86e69
Vitalii Oleksenko <oanatol492@gmail.com>
2023-05-02 00:47:25 +0300

    clean up ExcelService.java

--- src/main/java/org/geekhub/vitalii/service/ExcelService.java
+++ src/main/java/org/geekhub/vitalii/service/ExcelService.java
@@ -1,5 +1,12 @@
 package org.geekhub.vitalii.service;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -9,13 +16,8 @@ import org.geekhub.vitalii.dto.UserStockDTO;
 import org.geekhub.vitalii.repository.PortfolioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import yahoofinance.Stock;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import yahoofinance.Stock;
 
 @Service
 public class ExcelService {
@@ -28,37 +30,34 @@ public class ExcelService {
     }
 
     public void exportToExcel(HttpServletResponse response, String username) {
-        LocalDate creationTime = LocalDate.now();
-        response.setContentType("application/vnd.ms-excel");
-        response.setHeader("Content-Disposition", "attachment; filename=stocks" + creationTime + ".xlsx");
-
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Stocks");
-
         List<ExcelCustomerStockDTO> customerStocks = getCustomerStocks(username);
 
-        int rowNum = 0;
-        Row header = sheet.createRow(rowNum++);
-        header.createCell(0).setCellValue("symbol");
-        header.createCell(1).setCellValue("name");
-        header.createCell(2).setCellValue("type");
-        header.createCell(3).setCellValue("amount");
-        header.createCell(4).setCellValue("value");
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=portfolio" + LocalDate.now() + ".xlsx");
 
-        for (ExcelCustomerStockDTO stock : customerStocks) {
-            Row row = sheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(stock.getSymbol());
-            row.createCell(1).setCellValue(stock.getName());
-            row.createCell(2).setCellValue(stock.getType());
-            row.createCell(3).setCellValue(stock.getAmount());
-            row.createCell(4).setCellValue(stock.getValue());
-        }
+        try (Workbook workbook = new XSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet("Stocks");
 
-        try {
+            int rowNum = 0;
+            Row header = sheet.createRow(rowNum++);
+            header.createCell(0).setCellValue("symbol");
+            header.createCell(1).setCellValue("name");
+            header.createCell(2).setCellValue("type");
+            header.createCell(3).setCellValue("amount");
+            header.createCell(4).setCellValue("value");
+
+            for (ExcelCustomerStockDTO stock : customerStocks) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(stock.getSymbol());
+                row.createCell(1).setCellValue(stock.getName());
+                row.createCell(2).setCellValue(stock.getType());
+                row.createCell(3).setCellValue(stock.getAmount());
+                row.createCell(4).setCellValue(stock.getValue());
+            }
+
             workbook.write(response.getOutputStream());
-            workbook.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to export data to Excel.", e);
         }
     }
 
@@ -71,7 +70,7 @@ public class ExcelService {
 
             double value = customerStock.getAmount().doubleValue() * stock.getQuote().getPrice().doubleValue();
             excelStocks.add(new ExcelCustomerStockDTO(customerStock.getSymbol(), stock.getName(), customerStock.getType(),
-                customerStock.getAmount().doubleValue(), value));
+                    customerStock.getAmount().doubleValue(), value));
         }
 
         return excelStocks;
